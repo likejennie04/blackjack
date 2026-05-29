b4deaf57b9fd600931b4ef9e3bf7a63f8155cae4
likejennie04 <aa1957945212@gmail.com>
2026-05-30 00:04:23 +0900

    Delete House.java

--- House.java
+++
@@ -1,6 +0,0 @@
-public class House extends Hand {
-    public String getInitialView() {
-        if (handCards.size() < 2) return "";
-        return "HIDDEN, " + handCards.get(1).toString();
-    }
-}
